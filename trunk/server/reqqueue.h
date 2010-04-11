9cd80374e27b832ffb8a171d76bc68b965bc13d0
mil.vis@seznam.cz <mil.vis@seznam.cz@c9bace2a-222a-11df-9371-19cf4aec4f78>
2010-04-11 00:23:41 +0000

    Komponenty serveru (v.1): Fronta pozadavku, Item Fronty
    Vcetne dokumentace v hlavickovych dokumentech.

---
+++ trunk/server/reqqueue.h
@@ -0,0 +1,95 @@
+#ifndef REQQUEUE_H
+#define REQQUEUE_H
+
+#include <QQueue>
+#include <QObject>
+#include <exception>
+#include "queueitem.h"
+
+/**
+  * Header file of class representing priority queue for ftp requests. Simple requests like
+  * USER, LIST, NOOP, etc. are removed earlier than request for FILE. Alternating of simple
+  * and FILE commands is secured by counter of removed items from priority queue. After 10
+  * simple commands in one line will remove 1 item from FILE queue. Queue has limited size.
+  * Queue also can show first item in both queues or on specify index.
+  */
+class ReqQueue : public QObject
+{
+    Q_QOBJECT
+
+public:
+    ReqQueue(int length);
+    /**
+      * Put QueueItem to back of queue. Simple FTP commands are putted to priority queue.
+      */
+    void enque(QueueItem in);
+    /**
+      * Remove QueueItem from queue. First from priorityQueue then from normal queue.
+      * If count of removed QueueItem from priorityQueue is more than 10, next removed
+      * will be from normal queue.
+      */
+    QueueItem deque();
+    /**
+      * Return poiter to first QueueItem in priorityQueue with simple commands.
+      */
+    QueueItem headSimpleCommands();
+    /**
+      * Return poiter to first QueueItem in normal queue with requests for file.
+      */
+    QueueItem headFiles();
+    /**
+      * Return poiter in priority or normal queue on index. Values of queueIdentificator are
+      * 0 - normal queue and 1 - priority queue. Value of index is from 0 to count of specified
+      * queue(found by countItems(bool queueIdentificator)). If inserted index is out of bounds
+      * function will return last item.
+      */
+    QueueItem onPosition(bool queueIdentificator, unsigned int index);
+    /**
+      * Testing that both queues are empty.
+      */
+    bool isEmpty();
+    /**
+      * Testing that queue can accept new item. Specially for accepting new incoming
+      * connection and for return uncompleted file-send request.
+      */
+    bool isFull();
+    /**
+      * Return count of items in specified queue. Values of queueIdentificator are 0 - normal
+      * queue and 1 - priority queue.
+      */
+    int countItems(bool queueIdentificator);
+
+public slots:
+signals:
+private:
+    /**
+      * Store queue of QueueItems contains simple FTP commands, e.g. LIST.
+      */
+    QQueue priorityQueue<QueueItem>;
+    /**
+      * Store queue of QueueItems contains requests for file.
+      */
+    QQueue queue<QueueItem>;
+    /**
+      * Size of all queue.
+      */
+    unsigned size;
+    /**
+      * Count of requests in queues together.
+      */
+    unsigned int count;
+    /**
+      * Count of requests in priority queue.
+      */
+    unsigned int countPriority;
+    /**
+      * Count of requests in normal queue.
+      */
+    unsigned int countNormal;
+    /**
+      * Max count of priority request which are served before take one normal request.
+      */
+    unsigned int priorityServed;
+};
+
+#endif // REQQUEUE_H
