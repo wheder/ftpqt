72ec56a4e59695182d748db8b0820a12112a583b
slusta@centrum.cz <slusta@centrum.cz@c9bace2a-222a-11df-9371-19cf4aec4f78>
2010-04-14 22:42:29 +0000

    jeste drobna uprava

--- trunk/FTPClient/ui_connection.h
+++ trunk/FTPClient/ui_connection.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'connection.ui'
 **
-** Created: Wed 14. Apr 23:42:11 2010
+** Created: Thu 15. Apr 00:12:50 2010
 **      by: Qt User Interface Compiler version 4.6.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
